a8dafbbfb0b7715535e26fab90e21a720fecd673
Andrei Lisovskiy <lis412@gmail.com>
2020-09-28 23:58:49 +0300

    feat: implement function for the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) return 0;
+  return array.reduce((res, value) => res < value ? res : value);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    return array.reduce((res, value) => res > value ? res : value);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    return array.reduce((res, value) => res + value) / array.length;
 }
